c859e1c795b78308b7fb29f7400e30d3c8d5250a
guintosopiamae711 <guintosopiamae@gmail.com>
2026-04-17 10:40:10 +0800

    Update login_system.py

--- April10/Week03_Act/Week02_Act/login_system.py
+++ April10/Week03_Act/Week02_Act/login_system.py
@@ -1,14 +1,14 @@
-correct_username_nbs = "admin"
-correct_password_nbs = "1234"
-attempts_nbs = 0
-while attempts_nbs < 3:
-    username_nbs = input("Enter username: ")
-    password_nbs = input("Enter password: ")
-    if username_nbs == correct_username_nbs and password_nbs == correct_password_nbs:
+correct_username_smg = "admin"
+correct_password_smg = "1234"
+attempts_smg = 0
+while attempts_smg < 3:
+    username_smg = input("Enter username: ")
+    password_smg = input("Enter password: ")
+    if username_smg == correct_username_smg and password_smg == correct_password_smg:
         print("Login Successful")
         break
     else:
         print("Invalid Credentials")
-        attempts_nbs += 1
-if attempts_nbs == 3:
+        attempts_smg += 1
+if attempts_smg == 3:
     print("Account Locked")
