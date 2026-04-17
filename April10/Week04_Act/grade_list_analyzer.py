5abc4ef5b75e0a217573fb9de95676c99bd9d20d
guintosopiamae711 <guintosopiamae@gmail.com>
2026-04-17 10:33:21 +0800

    Update grade_list_analyzer.py

--- April10/Week04_Act/grade_list_analyzer.py
+++ April10/Week04_Act/grade_list_analyzer.py
@@ -1,19 +1,20 @@
 print("----- GRADE LIST ANALYZER -----")
-grades_nbs = []
+grades_smg = []
 
 # Ask user to enter 5 grades
-for i in range(1, 6):
-    grade = float(input(f"Enter grade {i}: "))
-    grades_nbs.append(grade)
+for i_smg in range(1, 6):
+    grade_smg = float(input(f"Enter grade {i_smg}: "))
+    grades_smg.append(grade_smg)
 
 # Compute statistics
-average_grade_nbs = sum(grades_nbs) / len(grades_nbs)
-highest_grade_nbs = max(grades_nbs)
-lowest_grade_nbs = min(grades_nbs)
+average_grade_smg = sum(grades_smg) / len(grades_smg)
+highest_grade_smg = max(grades_smg)
+lowest_grade_smg = min(grades_smg)
 
 # Display results
 print("\n----- RESULTS -----")
-print("Grades:", grades_nbs)
-print("Average Grade:", round(average_grade_nbs, 1))
-print("Highest Grade:", highest_grade_nbs)
-print("Lowest Grade:", lowest_grade_nbs)
+print("Grades:", grades_smg)
+print("Average Grade:", round(average_grade_smg, 1))
+print("Highest Grade:", highest_grade_smg)
+print("Lowest Grade:", lowest_grade_smg)
+
