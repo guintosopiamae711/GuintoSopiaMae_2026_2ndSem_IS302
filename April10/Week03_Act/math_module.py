7779ff13e69454b2a6485c7d17244318052114b2
guintosopiamae711 <guintosopiamae@gmail.com>
2026-04-17 10:31:25 +0800

    Update math_module.py

--- April10/Week03_Act/math_module.py
+++ April10/Week03_Act/math_module.py
@@ -1,11 +1,11 @@
-def add(a_nbs, b_nbs):
-    return a_nbs + b_nbs
+def add(a_smg, b_smg):
+    return a_smg + b_smg
 
-def subtract(a_nbs, b_nbs):
-    return a_nbs - b_nbs
+def subtract(a_smg, b_smg):
+    return a_smg - b_smg
 
-def multiply(a_nbs, b_nbs):
-    return a_nbs * b_nbs
+def multiply(a_smg, b_smg):
+    return a_smg * b_smg
 
-def divide(a_nbs, b_nbs):
-    return a_nbs / b_nbs
+def divide(a_smg, b_smg):
+    return a_smg / b_smg
