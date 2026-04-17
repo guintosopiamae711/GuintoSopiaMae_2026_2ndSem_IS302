a4e13caad421465c7ed0184a069b235498424532
guintosopiamae711 <guintosopiamae@gmail.com>
2026-04-17 10:34:39 +0800

    Update student_list.py

--- April10/Week04_Act/student_list.py
+++ April10/Week04_Act/student_list.py
@@ -1,7 +1,7 @@
-students_nbs = []
-for i in range(5):
-    name = input("Enter student name: ")
-    students_nbs.append(name)
+students_smg = []
+for i_smg in range(5):
+    name_smg = input("Enter student name: ")
+    students_smg.append(name_smg)
 print("\nStudent List:")
-for student_nbs in students_nbs:
-    print(student_nbs)
+for student_smg in students_smg:
+    print(student_smg)
