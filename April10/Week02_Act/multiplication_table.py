0a97b703ce29322a3eb9b05c9d0ce759544462df
guintosopiamae711 <guintosopiamae@gmail.com>
2026-04-17 10:24:42 +0800

    Update multiplication_table.py

--- April10/Week02_Act/multiplication_table.py
+++ April10/Week02_Act/multiplication_table.py
@@ -1,3 +1,3 @@
-number_nbs = int(input("Enter a number: "))
-for i_nbs in range(1, 11):
-    print(number_nbs, "x", i_nbs, "=", number_nbs * i_nbs)
+number_smg = int(input("Enter a number: "))
+for i_smg in range(1, 11):
+    print(number_smg, "x", i_smg, "=", number_smg * i_smg)
