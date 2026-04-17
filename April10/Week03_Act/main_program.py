aa03ff662ebb43fb231967974eb637ca14d91c42
guintosopiamae711 <guintosopiamae@gmail.com>
2026-04-17 10:30:06 +0800

    Update main_program.py

--- April10/Week03_Act/main_program.py
+++ April10/Week03_Act/main_program.py
@@ -1,8 +1,8 @@
 import math_module
 
-num1_nbs = float(input("Enter first number: "))
-num2_nbs = float(input("Enter second number: "))
-print("Addition:", math_module.add(num1_nbs, num2_nbs))
-print("Subtraction:", math_module.subtract(num1_nbs, num2_nbs))
-print("Multiplication:", math_module.multiply(num1_nbs, num2_nbs))
-print("Division:", math_module.divide(num1_nbs, num2_nbs))
+num1_smg = float(input("Enter first number: "))
+num2_smg = float(input("Enter second number: "))
+print("Addition:", math_module.add(num1_smg, num2_smg))
+print("Subtraction:", math_module.subtract(num1_smg, num2_smg))
+print("Multiplication:", math_module.multiply(num1_smg, num2_smg))
+print("Division:", math_module.divide(num1_smg, num2_smg))
